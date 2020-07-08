9e0b9a09b5646826d611f2b4151d4a0852986812
Kevin <kevin@rocher-dev.com>
2020-07-08 10:07:21 +0200

    v 2.2.8
    * Add a hook for a classname on « logos » block.

--- blocks/logos/content.php
+++ blocks/logos/content.php
@@ -20,11 +20,14 @@ while (has_sub_field('logos')) {
 }
 $_content_before = apply_filters('wpu_acf_flexible__content__logos__before', '');
 $_content_after = apply_filters('wpu_acf_flexible__content__logos__after', '');
+
+$logos_list_classname = apply_filters('wpu_acf_flexible__content__logos__logoslist__classname', 'logos-list');
+
 ?><div class="<?php echo get_wpu_acf_wrapper_classname('logos'); ?>">
     <?php echo $_content_before; ?>
     <div class="block--logos">
         <?php echo get_wpu_acf_title_content(); ?>
-        <ul class="logos-list">
+        <ul class="<?php echo $logos_list_classname; ?>">
             <?php echo $_logos; ?>
         </ul>
     </div>
