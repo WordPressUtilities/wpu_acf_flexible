5ef731092b2f67452d96dc77387d2160554e8aa0
Kevin <kevin@rocher-dev.com>
2021-05-19 21:32:58 +0200

    v 2.13.3
    - Multiple links on classic content block.

--- blocks/content-classic/model.php
+++ blocks/content-classic/model.php
@@ -12,8 +12,19 @@ $model = array(
             'type' => 'editor',
             'toolbar' => 'full'
         ),
+        'cola' => 'wpuacf_33p',
         'cta' => array(
-            'label' => __('Button', 'wpu_acf_flexible'),
+            'label' => sprintf(__('Button %s', 'wpu_acf_flexible'),1),
+            'type' => 'link'
+        ),
+        'colb' => 'wpuacf_33p',
+        'cta2' => array(
+            'label' => sprintf(__('Button %s', 'wpu_acf_flexible'),2),
+            'type' => 'link'
+        ),
+        'colc' => 'wpuacf_33p',
+        'cta3' => array(
+            'label' => sprintf(__('Button %s', 'wpu_acf_flexible'),3),
             'type' => 'link'
         )
     )
