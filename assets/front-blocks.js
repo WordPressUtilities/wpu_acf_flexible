428e78745365fbd002919cb8be57e5829049df07
Kevin <kevin@rocher-dev.com>
2022-02-16 14:53:23 +0100

    v 2.20.1
    - Fix click behavior on videos.

--- assets/front-blocks.js
+++ assets/front-blocks.js
@@ -2,20 +2,24 @@ window.addEventListener("DOMContentLoaded", function(e) {
 
     /* Video blocks */
     (function() {
-        var $videos = document.querySelectorAll('.wpuacf-video');
-        for (var i = 0, len = $videos.length; i < len; i++) {
-            set_play_event($videos[i]);
-        }
+
+        /* Check click event on every possible video (loaded ) */
+        document.body.addEventListener('click', function(e) {
+            for (var target = e.target; target && target != this; target = target.parentNode) {
+                if (target.matches('.wpuacf-video')) {
+                    set_play_event(target);
+                    break;
+                }
+            }
+        });
 
         function set_play_event($video) {
-            $video.addEventListener('click', function() {
-                var $iframe = $video.querySelector('iframe');
-                $iframe.setAttribute('src', $iframe.getAttribute('data-src'));
-                $video.setAttribute('data-is-loading', 1);
-                setTimeout(function() {
-                    $video.setAttribute('data-is-playing', 1);
-                }, 500);
-            }, false);
+            var $iframe = $video.querySelector('iframe[data-src]');
+            $iframe.setAttribute('src', $iframe.getAttribute('data-src'));
+            $video.setAttribute('data-is-loading', 1);
+            setTimeout(function() {
+                $video.setAttribute('data-is-playing', 1);
+            }, 500);
         }
     }());
 
