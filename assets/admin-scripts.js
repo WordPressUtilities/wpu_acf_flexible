86c2e91893fdea2c26940b9cddc0076029868890
Kevin <kevin@rocher-dev.com>
2022-10-08 21:53:58 +0200

    v 2.30.2
    - Fix for duplicated unique IDs

--- assets/admin-scripts.js
+++ assets/admin-scripts.js
@@ -12,4 +12,39 @@ document.addEventListener("DOMContentLoaded", function() {
             $langs[i].click();
         }
     }());
+
+    /* Create random field */
+    (function() {
+        if (typeof acf == "undefined") {
+            return;
+        }
+
+        function _field_set_random_value(_field) {
+            _field.val('hidden_' + Math.random().toString(36).slice(2, 12) + Date.now());
+        }
+
+        /* Set initial random value */
+        acf.addAction('new_field', function(field) {
+            if (field.$el.hasClass('wpu-acf-flex-hidden-field')) {
+                var _tmpField = field.$el.find('input[name*="acf"]');
+                if (!_tmpField.val()) {
+                    _field_set_random_value(_tmpField);
+                }
+            }
+        });
+        /* On duplicate : randomize new value */
+        acf.addAction('append', function($el) {
+            if (!$el.hasClass('layout')) {
+                return;
+            }
+            var $hiddenFields = $el.find('.wpu-acf-flex-hidden-field input[name*="acf"]');
+            if (!$hiddenFields.length) {
+                return;
+            }
+            $hiddenFields.each(function() {
+                _field_set_random_value(jQuery(this));
+            });
+        });
+    }());
+
 });
