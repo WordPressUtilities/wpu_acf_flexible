d6571f53a93b080354e3fb0f77a3b00d41ae69ac
Kevin <kevin@rocher-dev.com>
2023-02-01 14:22:36 +0100

    v 2.36.1
    - Better preview for block type anchor.

--- blocks/anchor/content.php
+++ blocks/anchor/content.php
@@ -3,4 +3,8 @@ $_anchor = preg_replace('/([^a-z0-9-])/', '', remove_accents(get_sub_field('slug
 if (!$_anchor) {
     return;
 }
+if (is_admin()) {
+    echo '<p style="padding:1em">#' . $_anchor . '</p>';
+    return;
+}
 ?><div id="<?php echo esc_attr($_anchor); ?>" class="<?php echo get_wpu_acf_wrapper_classname('anchor'); ?>"></div>
