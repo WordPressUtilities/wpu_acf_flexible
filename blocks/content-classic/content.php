6578b16601d6f27aebb90c70c2ef011ce00330c7
Kevin <kevin@rocher-dev.com>
2023-11-23 22:22:27 +0100

    v 2.48.4
    - New hooks for content classic block.

--- blocks/content-classic/content.php
+++ blocks/content-classic/content.php
@@ -1,4 +1,9 @@
 <?php
+
+/* ----------------------------------------------------------
+  Values
+---------------------------------------------------------- */
+
 $_content = get_wpu_acf_title_content();
 $_cta_link = get_wpu_acf_cta();
 if (!$_content && !$_cta_link) {
@@ -10,10 +15,23 @@ for ($i = 2; $i <= $nb_buttons; $i++) {
     $_cta_link .= get_wpu_acf_cta('cta' . $i);
 }
 
+/* ----------------------------------------------------------
+  Extra hooks
+---------------------------------------------------------- */
+
 $_content_before = apply_filters('wpu_acf_flexible__content__content_classic__before', '');
 $_content_after = apply_filters('wpu_acf_flexible__content__content_classic__after', '');
-?><div class="<?php echo get_wpu_acf_wrapper_classname('content-classic'); ?>">
-    <?php echo $_content_before; ?>
-    <div class="block--content-classic"><?php echo $_content . $_cta_link; ?></div>
-    <?php echo $_content_after; ?>
-</div>
+$_content_inside_before = apply_filters('wpu_acf_flexible__content__content_classic__inside_before', '');
+$_content_inside_after = apply_filters('wpu_acf_flexible__content__content_classic__inside_after', '');
+
+/* ----------------------------------------------------------
+  Content
+---------------------------------------------------------- */
+
+echo '<div class="' . get_wpu_acf_wrapper_classname('content-classic') . '">';
+echo $_content_before;
+echo '<div class="block--content-classic">';
+echo $_content_inside_before . $_content . $_cta_link . $_content_inside_after;
+echo '</div>';
+echo $_content_after;
+echo '</div>';
