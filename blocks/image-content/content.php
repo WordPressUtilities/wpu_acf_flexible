2b3cbda32a6eb0be2c22a214ef29f0637efd9a78
Kevin <kevin@rocher-dev.com>
2024-03-01 17:26:23 +0100

    v 2.55.2
    - New hooks in Image - content block.

--- blocks/image-content/content.php
+++ blocks/image-content/content.php
@@ -17,19 +17,27 @@ $_image_container = '<div class="main-grid__image">' . $_image . '</div>';
 $_title_position = apply_filters('wpu_acf_flexible__content__image_content__title_position', 'inner');
 $_content_before = apply_filters('wpu_acf_flexible__content__image_content__before', '');
 $_content_after = apply_filters('wpu_acf_flexible__content__image_content__after', '');
-?><div data-image-position="<?php echo esc_attr($_image_position); ?>" class="<?php echo get_wpu_acf_wrapper_classname('image-content'); ?>">
-    <?php echo $_content_before; ?>
-    <div class="block--image-content">
-        <?php echo $_title_position == 'over' ? get_wpu_acf__title() : ''; ?>
-        <div class="acfflex-grid">
-        <?php echo $_image_position == 'left' ? $_image_container : ''; ?>
-        <div class="main-grid__content">
-            <?php echo $_title_position == 'inner' ? get_wpu_acf__title() : ''; ?>
-            <?php echo get_wpu_acf__content(); ?>
-            <?php echo get_wpu_acf_cta(); ?>
-        </div>
-        <?php echo $_image_position == 'right' ? $_image_container : ''; ?>
-        </div>
-    </div>
-    <?php echo $_content_after; ?>
-</div>
+$_content_before_main = apply_filters('wpu_acf_flexible__content__image_content__main__before', '');
+$_content_after_main = apply_filters('wpu_acf_flexible__content__image_content__main__after', '');
+
+echo '<div data-image-position="' . esc_attr($_image_position) . '" class="' . get_wpu_acf_wrapper_classname('image-content') . '">';
+echo $_content_before;
+echo '<div class="block--image-content">';
+
+echo $_title_position == 'over' ? get_wpu_acf__title() : '';
+
+echo '<div class="acfflex-grid">';
+echo $_image_position == 'left' ? $_image_container : '';
+echo '<div class="main-grid__content">';
+echo $_content_before_main;
+echo $_title_position == 'inner' ? get_wpu_acf__title() : '';
+echo get_wpu_acf__content();
+echo get_wpu_acf_cta();
+echo $_content_after_main;
+echo '</div>';
+echo $_image_position == 'right' ? $_image_container : '';
+echo '</div>';
+
+echo '</div>';
+echo $_content_after;
+echo '</div>';
