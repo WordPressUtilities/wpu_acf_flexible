1bf6660d174532a20ff308d4b29a436e4e5bc401
Kevin <kevin@rocher-dev.com>
2024-11-19 21:38:46 +0100

    v 2.74.3
    Display block usage

--- inc/reusable-blocks.php
+++ inc/reusable-blocks.php
@@ -32,6 +32,41 @@ add_action('init', function () {
 });
 
 /* ----------------------------------------------------------
+  Display block usage
+---------------------------------------------------------- */
+
+add_action('add_meta_boxes', function () {
+    if (!apply_filters('wpu_acf_flexible__enable_reusable_blocks', false)) {
+        return;
+    }
+    add_meta_box('wpuacf_blocks', __('Block usage', 'wpu_acf_flexible'), function ($post) {
+        global $wpdb;
+        $q = "
+        SELECT *  FROM $wpdb->posts
+            WHERE
+            post_status NOT IN('inherit')
+            AND ID IN(
+                SELECT post_id FROM $wpdb->postmeta
+                WHERE
+                    meta_key LIKE '%wpuacf_blocks'
+                    AND meta_value = " . $post->ID . "
+            )
+            ORDER BY ID ASC
+        ";
+        $metas = $wpdb->get_results($q);
+        if (!is_array($metas)) {
+            return;
+        }
+        echo '<ul>';
+        foreach ($metas as $meta) {
+            $p = get_post($meta->ID);
+            echo '<li><a href="' . get_edit_post_link($meta->ID) . '">' . $p->post_title . '</a> (' . $p->post_type . ')</li>';
+        }
+        echo '</ul>';
+    }, 'wpuacf_blocks', 'normal', 'high');
+});
+
+/* ----------------------------------------------------------
   Add block model
 ---------------------------------------------------------- */
 
