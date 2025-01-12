764e6722a9916f3e85455bf382f2c801d0d1ca3d
Kevin <kevin@rocher-dev.com>
2025-01-12 22:17:36 +0100

    v 2.77.1
    - new hooks on faq type content

--- blocks/faq/content.php
+++ blocks/faq/content.php
@@ -5,18 +5,21 @@ if (!$questions) {
     return;
 }
 $_content_before = apply_filters('wpu_acf_flexible__content__faq__before', '');
+$_content_before_list = apply_filters('wpu_acf_flexible__content__faq__before_list', '');
 $_content_after = apply_filters('wpu_acf_flexible__content__faq__after', '');
 echo '<section class="' . get_wpu_acf_wrapper_classname('faq') . '">';
 echo $_content_before;
 echo '<div class="block--faq">';
 echo get_wpu_acf_title_content();
+echo $_content_before_list;
 echo '<div class="faq-list__items">';
 while (have_rows('questions')):
     the_row();
     $answer = get_sub_field('answer');
     $faq_id = sanitize_title('faq-' . $answer) . uniqid();
     $question = get_sub_field('question');
-    echo '<dl class="faq-list__item wpuacfflexfaq-list__item" itemscope="" itemtype="https://schema.org/Question" data-is-open="false">';
+    $attributes = apply_filters('wpu_acf_flexible__content__faq__item_attributes', '', get_row());
+    echo '<dl ' . $attributes . ' class="faq-list__item wpuacfflexfaq-list__item" itemscope="" itemtype="https://schema.org/Question" data-is-open="false">';
     echo '<dt itemprop="name" class="h3 field-question"><button aria-expanded="false" aria-controls="' . esc_attr($faq_id) . '"><span>' . $question . '</span></button></dt>';
     echo '<dd id="' . esc_attr($faq_id) . '" class="block-answer" itemscope="" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">';
     echo '<div class="field-answer cssc-content" itemprop="text">' . trim(wpautop($answer)) . '</div>';
