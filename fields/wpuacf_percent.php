cc391535fc7e41006bf1f6ddbd4c3ff179819095
Kevin <kevin@rocher-dev.com>
2025-08-01 23:30:16 +0200

    v 2.90.1
    Field type percent.

---
+++ fields/wpuacf_percent.php
@@ -0,0 +1,25 @@
+<?php
+defined('ABSPATH') || die;
+
+/* ----------------------------------------------------------
+  Percent
+---------------------------------------------------------- */
+
+add_filter('wpu_acf_flexible__field_types', function ($types) {
+    $types['wpuacf_percent'] = array(
+        'label' => 'Percent',
+        'append' => '%',
+        'type' => 'number',
+        'default_value' => 100,
+        'min' => 0,
+        'max' => 100
+    );
+    return $types;
+}, 10, 1);
+
+function get_wpuacf_percent($field) {
+    if (!$field || !is_numeric($field)) {
+        return '';
+    }
+    return $field . '%';
+}
