1d47e913a4cb04b1b4d3957bcd41c1c13d513b06
Kevin <kevin@rocher-dev.com>
2026-01-11 21:53:55 +0100

    v 3.12.0
    More settings for iframe block.

--- blocks/iframe/content.php
+++ blocks/iframe/content.php
@@ -16,13 +16,38 @@ if (!is_numeric($_height)) {
 $mobile_height = get_sub_field('mobile_height');
 
 $row_id = 'iframe_' . wpuacfflex_get_row_id();
+$iframe_id = get_sub_field('iframe_id');
+if ($iframe_id) {
+    $row_id = trim(sanitize_html_class($iframe_id));
+}
+
+$iframe_attributes = array(
+    'id' => $row_id,
+    'src' => esc_url($_url),
+    'width' => '100%',
+    'height' => $_height,
+    'frameborder' => '0',
+    'allowTransparency' => 'true',
+    'style' => 'border: 0'
+);
+if (get_sub_field('iframe_title')) {
+    $iframe_attributes['title'] = get_sub_field('iframe_title');
+}
+if (get_sub_field('enable_lazy_loading')) {
+    $iframe_attributes['loading'] = 'lazy';
+}
+
+$iframe_attributes_string = '';
+foreach ($iframe_attributes as $attr => $value) {
+    $iframe_attributes_string .= ' ' . $attr . '="' . esc_attr($value) . '"';
+}
 
 $_content_before = apply_filters('wpu_acf_flexible__content__iframe__before', '');
 $_content_after = apply_filters('wpu_acf_flexible__content__iframe__after', '');
 echo '<div class="' . get_wpu_acf_wrapper_classname('iframe') . '">';
 echo $_content_before;
 echo '<div class="block--iframe">';
-echo '<iframe id="' . $row_id . '" src="' . esc_url($_url) . '" width="100%" height="' . $_height . '" frameborder="0" allowTransparency="true" style="border: 0"></iframe>';
+echo '<iframe' . $iframe_attributes_string . '></iframe>';
 echo '</div>';
 if ($mobile_height) {
     echo '<style>@media (max-width:768px) {#' . $row_id . '{height:' . $mobile_height . 'px}}</style>';
--- blocks/iframe/model.php
+++ blocks/iframe/model.php
@@ -20,6 +20,27 @@ $model = array(
             'label' => __('Iframe Height (Mobile)', 'wpu_acf_flexible'),
             'type' => 'number'
         ),
-        'colc' => 'wpuacf_100p'
+        'colc' => 'wpuacf_100p',
+        'extra_settings' => array(
+            'label' => __('Settings', 'wpu_acf_flexible'),
+            'type' => 'accordion',
+        ),
+        'iframe_id' => array(
+            'label' => __('Iframe ID', 'wpu_acf_flexible'),
+            'type' => 'text',
+            'instructions' => __('Optional ID attribute for the iframe', 'wpu_acf_flexible'),
+        ),
+        'iframe_title' => array(
+            'label' => __('Iframe Title', 'wpu_acf_flexible'),
+            'type' => 'text',
+            'instructions' => __('Optional title attribute for the iframe (for accessibility)', 'wpu_acf_flexible'),
+        ),
+        'enable_lazy_loading' => array(
+            'label' => __('Enable Lazy Loading', 'wpu_acf_flexible'),
+            'type' => 'true_false',
+            'ui' => 1,
+            'default_value' => 0,
+            'instructions' => __('If enabled, the iframe will load only when it enters the viewport.', 'wpu_acf_flexible'),
+        )
     )
 );
