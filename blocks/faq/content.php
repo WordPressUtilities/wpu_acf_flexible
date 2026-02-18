dcd1092159945c6c64a08385228e600022b5981a
Kevin <kevin@rocher-dev.com>
2026-02-18 11:40:56 +0100

    v 3.13.2
    Block FAQ :
    - Hooks for tags.

--- blocks/faq/content.php
+++ blocks/faq/content.php
@@ -8,6 +8,14 @@ $_content_before = apply_filters('wpu_acf_flexible__content__faq__before', '');
 $_content_before_title = apply_filters('wpu_acf_flexible__content__faq__before_title', '');
 $_content_before_list = apply_filters('wpu_acf_flexible__content__faq__before_list', '');
 $_content_after = apply_filters('wpu_acf_flexible__content__faq__after', '');
+
+$_faq_block_settings = apply_filters('wpu_acf_flexible__content__faq__block_settings', array(
+    'tag_wrapper' => 'div',
+    'tag_question' => 'dl',
+    'tag_question_title' => 'dt',
+    'tag_question_content' => 'dd'
+));
+
 $_block_attribute = '';
 if (!defined('WPUACFFLEXIBLE_BLOCK_FAQ_DISPLAYED')) {
     define('WPUACFFLEXIBLE_BLOCK_FAQ_DISPLAYED', true);
@@ -20,21 +28,21 @@ echo '<div class="block--faq" ' . $_block_attribute . '>';
 echo $_content_before_title;
 echo get_wpu_acf_title_content();
 echo $_content_before_list;
-echo '<div class="faq-list__items">';
+echo '<' . $_faq_block_settings['tag_wrapper'] . ' class="faq-list__items">';
 while (have_rows('questions')):
     the_row();
     $answer = get_sub_field('answer');
     $faq_id = sanitize_title('faq-' . $answer) . uniqid();
     $question = get_sub_field('question');
     $attributes = apply_filters('wpu_acf_flexible__content__faq__item_attributes', '', get_row());
-    echo '<dl ' . $attributes . ' class="faq-list__item wpuacfflexfaq-list__item" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question" data-is-open="false">';
-    echo '<dt itemprop="name" class="h3 field-question"><button aria-expanded="false" aria-controls="' . esc_attr($faq_id) . '"><span>' . $question . '</span></button></dt>';
-    echo '<dd id="' . esc_attr($faq_id) . '" class="block-answer" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">';
+    echo '<' . $_faq_block_settings['tag_question'] . ' ' . $attributes . ' class="faq-list__item wpuacfflexfaq-list__item" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question" data-is-open="false">';
+    echo '<' . $_faq_block_settings['tag_question_title'] . ' itemprop="name" class="h3 field-question"><button aria-expanded="false" aria-controls="' . esc_attr($faq_id) . '"><span>' . $question . '</span></button></' . $_faq_block_settings['tag_question_title'] . '>';
+    echo '<' . $_faq_block_settings['tag_question_content'] . ' id="' . esc_attr($faq_id) . '" class="block-answer" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">';
     echo '<div class="field-answer cssc-content" itemprop="text">' . trim(wpautop($answer)) . '</div>';
-    echo '</dd>';
-    echo '</dl>';
+    echo '</' . $_faq_block_settings['tag_question_content'] . '>';
+    echo '</' . $_faq_block_settings['tag_question'] . '>';
 endwhile;
-echo '</div>';
+echo '</' . $_faq_block_settings['tag_wrapper'] . '>';
 echo '</div>';
 echo $_content_after;
 echo '</section>';
